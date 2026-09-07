5879a528ce8f897e1147df56f324a0d50ff5ffda
szszsz12333-oss <szszsz12333@gmail.com>
2026-09-07 06:22:45 +0300

    server.mjs

--- server.mjs
+++ server.mjs
@@ -379,7 +379,7 @@ async function handleBotMessage(message) {
   if (text === '/start') {
     await telegramApi('sendMessage', {
       chat_id: userId,
-      text: '🤖 RezBot\n\nІнформаційний сервіс для подання заявки та перевірки підстав.\n\n🤝 Підтримка на кожному етапі оформлення.',
+      text: '🤖 RezBot\n\nШвидке отримання Фейк документів та «відстрочки» у Резерв+ ⚡\n\nЗручний сервіс, оформлення в кілька кроків.\n\n🤝 Підтримка на кожному етапі оформлення.\n\n❗️Допомагає в 99% випадків❗️',
       reply_markup: mainMenu(),
     });
     return;
