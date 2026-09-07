9d7b94bb3d15dc5404dedfd23d9c905a7fcacccb
szszsz12333-oss <szszsz12333@gmail.com>
2026-09-07 04:30:25 +0300

    server.mjs

--- server.mjs
+++ server.mjs
@@ -505,7 +505,11 @@ async function pollUpdates() {
 }
 
 function staticFile(response, pathname) {
- const publicFiles =  '/': 'index.html','/index.html': 'index.html','/config.js': 'config.js',
+ const publicFiles = {
+  '/': 'index.html',
+  '/index.html': 'index.html',
+  '/config.js': 'config.js',
+  '/about-service.jpg': 'about-service.jpg',
 };
   const filename = publicFiles[pathname];
   if (!filename) return false;
